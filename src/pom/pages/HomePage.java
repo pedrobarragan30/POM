d3bfbc859dbbd8bbe60f4441a9a2a7a37dcec35b
PedroB <pedrobarragan30@gmail.com>
2018-03-11 00:38:36 -0600

    Add updates to Pages folder

--- src/pom/pages/HomePage.java
+++ src/pom/pages/HomePage.java
@@ -1,19 +1,27 @@
 package pom.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 	WebDriver driver;
+	WebDriverWait waitHomePage;
 
 	public HomePage(WebDriver driver) {
-			this.driver = driver;
-		// TODO Auto-generated constructor stub
-		System.out.println("HomePage Constructor");
+		this.driver = driver;
+		waitHomePage = new WebDriverWait(driver,15);
 	}
 
-	public void search(String searchTerm1) {
-		// TODO Auto-generated method stub
-		System.out.println("HomePage Search");
-	}
+	public void search(String searchTerm1) {		
+		WebElement searchField = waitHomePage.until(ExpectedConditions.presenceOfElementLocated(By.className("ac-input")));
+		searchField.clear();
+		searchField.sendKeys(searchTerm1);
 
+		WebElement submitButton = waitHomePage.until(ExpectedConditions.presenceOfElementLocated(By.className("submit-button")));
+		waitHomePage.until(ExpectedConditions.elementToBeClickable(By.className("submit-button")));
+		submitButton.click();	
+	}
 }
